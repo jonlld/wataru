278775ac1d7f28fa60b7a953e9c3ba17e027dfdf
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-30 13:07:16 +0900

    add list db with two users

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
+# type import
+from typing import List
+from uuid import uuid4
+# FastAPI
 from fastapi import FastAPI
+# from our models.py
+from models import User, Gender, Role
 
 app = FastAPI()
+
+# what syntax is this? also, not highlighted...
+db: List[User] = [
+    User(
+        id=uuid4(),
+        first_name="Jamila",
+        last_name="Ahmed",
+        gender=Gender.female,
+        roles=[Role.student]
+    ),
+    User(
+        id=uuid4(),
+        first_name="Alex",
+        last_name="Jones",
+        gender=Gender.male,
+        roles=[Role.admin, Role.user]
+    )
+]
 
 
 @app.get("/")
