c9814d8b1ffa1bb93b74a02872e72c4ba3083d29
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-30 12:50:53 +0900

    add model using pydantic

---
+++ models.py
@@ -0,0 +1,31 @@
+# this model is using pydantic - for data validation and settings
+
+from typing import Optional, List
+from uuid import UUID, uuid4
+from pydantic import BaseModel
+from enum import Enum
+
+# User class will extend BaseModel
+# Inside class can add attributes
+# optional and data type is UUID, if not provided use uuid4
+# gender will be an Enum, which is choices we specify? import Enum and pass in class name to the attribute
+
+
+class Gender(str, Enum):
+    male = "male"
+    female = "female"
+
+
+class Role(str, Enum):
+    admin = "admin"
+    user = "user"
+    student = "student"
+
+
+class User(BaseModel):
+    id: Optional[UUID] = uuid4
+    first_name: str
+    last_name: str
+    middle_name: Optional[str]
+    gender: Gender
+    roles: List[Role]
