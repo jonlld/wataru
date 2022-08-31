08d5320b24fbe057a9aae5428ddc56cb2689d1c0
jonlld <87318928+jonlld@users.noreply.github.com>
2022-08-31 18:33:05 +0900

    simplify file and update names for phrasebook api

--- main.py
+++ main.py
@@ -2,103 +2,19 @@
 from http.client import HTTPException
 from typing import List
 from uuid import UUID, uuid4
+
 import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 
-# FastAPI
 from fastapi import FastAPI, HTTPException
-
-# from our models.py
-from models import User, Gender, Role, UserUpdateRequest
+from models import UserRequest, Phrase
 
 app = FastAPI()
 
-# what syntax is this? also, not highlighted...
-db: List[User] = [
-    User(
-        # id=uuid4(),
-        id=UUID("c7a09d0b-ac43-4ab5-ab1a-55c626fe0953"),
-        first_name="Jamila",
-        last_name="Ahmed",
-        gender=Gender.female,
-        roles=[Role.student],
-    ),
-    User(
-        id=UUID("c0975c1e-82fa-4343-b0a0-654f077f23cb"),
-        first_name="Alex",
-        last_name="Jones",
-        gender=Gender.male,
-        roles=[Role.admin, Role.user],
-    ),
-]
+phrases = []
 
-
-@app.get("/")
-def root():
-    return {"hi": "there"}
-
-
-# simple endpoint to serve users list
-
-
-@app.get("/api/v1/users")
-async def fetch_users():
-    return db
-
-
-# submit a new user - same endpoint, different method
-# will receive a user of type User
-
-
-@app.post("/api/v1/users")
-async def register_user(user: User):
-    # takes user from req body and appends
-    db.append(user)
-    # send id back to client
-    return {"id": user.id}
-
-
-# delete user endpoint
-# using a 'path variable'
-
-
-@app.delete("/api/v1/users/{user_id}")
-async def delete_user(user_id: UUID):
-    for user in db:
-        if user.id == user_id:
-            db.remove(user)
-            return
-    raise HTTPException(
-        status_code=404, detail=f"user with id: {user_id} does not exist"
-    )
-
-
-# update user - payload client sends will be user_update with type UserUpdateRequest (all optional)
-# take the user_update (with update class) and user_id from path
-
-
-@app.put("/api/v1/users/{user_id}")
-async def update_user(user_update: UserUpdateRequest, user_id: UUID):
-    for user in db:
-        if user.id == user_id:
-            if user_update.first_name is not None:
-                user.first_name = user_update.first_name
-            if user_update.last_name is not None:
-                user.last_name = user_update.last_name
-            if user_update.middle_name is not None:
-                user.middle_name = user_update.middle_name
-            if user_update.roles is not None:
-                user.roles = user_update.roles
-            return
-
-    # if user not found
-    raise HTTPException(
-        status_code=404, detail=f"user with id: {user_id} does not exist"
-    )
-
-
-# sample code to test API
+# Google Translate - Client API
 def translate_text(target, text):
     import six
     from google.cloud import translate_v2 as translate
@@ -114,6 +30,34 @@ def translate_text(target, text):
     print("Translation: {}".format(result["translatedText"]))
     print("Detected source language: {}".format(result["detectedSourceLanguage"]))
 
+    return result
 
-translate_text("ja", "This is a test")
-translate_text("ja", "Yesterday's sunset was really beautiful")
+
+@app.get("/api/phrases")
+def fetch_all():
+    return phrases
+
+
+# @app.get("/phrases/request")
+# async def add_phrases(phrase: UserRequest):
+#     return phrases
+
+
+# doesn't work if I include await for translate function!
+@app.post("/api/phrases/request")
+async def translate_request(req: UserRequest):
+    print(req)
+    target = req.request_lang
+    text = req.request_string
+    translated = translate_text(target, text)
+    phrases.append(translated)
+    return phrases
+
+
+# [
+#   {
+#     "translatedText": "車",
+#     "detectedSourceLanguage": "en",
+#     "input": "car"
+#   }
+# ]
--- models.py
+++ models.py
@@ -1,44 +1,18 @@
-# this model is using pydantic - for data validation and settings
-
 from typing import Optional, List
 from uuid import UUID, uuid4
 from pydantic import BaseModel
-from enum import Enum
 
-# User class will extend BaseModel
-# Inside class can add attributes
-# optional and data type is UUID, if not provided use uuid4
-# gender will be an Enum, which is choices we specify? import Enum and pass in class name to the attribute
-# if doesn't have Optional, is not nullable
-# id is automatically the pk
-# roles will be a list of strings, from the options in Role
-
-
-class Gender(str, Enum):
-    male = "male"
-    female = "female"
-
-
-class Role(str, Enum):
-    admin = "admin"
-    user = "user"
-    student = "student"
-
-
-class User(BaseModel):
-    id: Optional[UUID] = uuid4()
-    first_name: str
-    last_name: str
-    middle_name: Optional[str]
-    gender: Gender
-    roles: List[Role]
+# Class will extend BaseModel
+# Add attributes
+class UserRequest(BaseModel):
+    request_string: str
+    request_lang: str
 
 
 # make a new class, all optional for update
 # as put requests will be in this format
 
-class UserUpdateRequest(BaseModel):
-    first_name: Optional[str]
-    last_name: Optional[str]
-    middle_name: Optional[str]
-    roles: Optional[List[Role]]
+
+class Phrase(BaseModel):
+    text: str
+    translation: str
